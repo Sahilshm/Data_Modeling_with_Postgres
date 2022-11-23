ad2cfc0aeb1f9a9926364717192c2adc255f97cd
Sahil Sharma <sahilshm93@gmail.com>
2022-11-23 13:14:55 +0530

    Code Enhancements

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# Data_Modeling_with_Postgres
- In this project we will build an ETL pipeline using Python for Data modeling with Postgres database.
+# Data Modeling with Postgres Project
+
+## Purpose of the project
+---
+The purpose of this project is to create a Postgres database that simplifies the querying of data on songs and user activity for the analytics team.
+
+## Database Schema Design
+---
+* The database schema used for this project is the star schema. 
+* It consists of one Fact table (song_play) and four Dimension Tables (users, songs artists and time). 
+* This schema design helps with simplifying complex queries and allows for greater performance when reading data from the database.
+
+## Postgres Database
+---
+The postgres db was set on top of Docker.
+Docker needs to be set up before executing the pipeline and then connections needs to updated accordingly.
+Postgres:Alpine was used for this project.
+
+## File Structure
+---
+The files in the repository are as follows:
+
+###### sql_queries.py
+This contains the queries that drops the tables if they exist, creates the tables, inserts extracted data from the datasets to the tables and a select query that gets the songid and artistid.
+
+###### create_tables.py
+This file contains the methods that execute the drop and create table queries found in the sql_queries file .
+
+###### etl.py
+Extracts data from the data files and inserts them in the database tables.
+
+###### init.ipynb
+Execute this file initially to install all pre-requisite libraries.
+
+###### test.ipynb
+This file can be used for testing the data in Postgres db once the pipeline finishes.
+
+## Running Scripts
+---
+###### Requirements
+`Python 3`
+`Postgresql`
+To drop or create the tables, ensure you are in the project's directory then run the following command:
+
+`python create_tables.py`
+To run the ETL pipeline that will extract data from the datasets and store them in the tables simply run:
+
+`python etl.py`
--- etl.py
+++ etl.py
@@ -1,1 +1,101 @@
 # Reads and processes files from song_data and log_data and loads them into your tables. You can fill this out based on your work in the ETL notebook.
+
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+
+def process_song_file(cur, filepath):
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+
+    # insert song record
+    song_data = df[['song_id', 'title','artist_id','year','duration']].values.tolist()[0]
+    cur.execute(songs_insert, song_data)
+    
+    # insert artist record
+    artist_data = df[['artist_id', 'artist_name','artist_location','artist_latitude','artist_longitude']].values.tolist()[0]
+    # print(artist_data)
+    cur.execute(artists_insert, artist_data)
+
+
+def process_log_file(cur, filepath):
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+
+    # filter by NextSong action
+    df = pd.read_json(filepath, lines=True)
+    next_song = df['page'] == "NextSong"
+
+    # convert timestamp column to datetime
+    t = df[next_song].copy(deep=True)
+    t['ts'] = pd.to_datetime(df['ts'])
+    
+    # insert time data records
+    time_data = ([t['ts'], t['ts'].dt.hour, t['ts'].dt.day, t['ts'].dt.isocalendar().week, t['ts'].dt.month, t['ts'].dt.year, t['ts'].dt.day_name() ])
+    column_labels = (['timestamp', 'hour','day', 'week_of_year','month', 'year', 'weekday'])
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_insert, list(row))
+
+    # load user table
+    user_df = t.filter(['userId','firstName','lastName','gender','level']).drop_duplicates()
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(users_insert, row)
+
+    # insert songplay records
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = ([pd.to_datetime(row.ts), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent])
+        cur.execute(songplays_insert, songplay_data)
+
+
+def process_data(cur, conn, filepath, func):
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    # print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        # print('{}/{} files processed.'.format(i, num_files))
+
+# Main Funcation
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=sparkify password=pass123")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/data_files/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/data_files/logs', func=process_log_file)
+
+    conn.close()
+
+# Executing Main
+
+if __name__ == "__main__":
+    main()
+    print("Data Load Successful !!!")
